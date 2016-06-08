4af01c491896fb944c8e63a9fe68b857aaf6ad30
Rambo Wu <RamboWu90@gmail.com>
2016-06-08 14:28:10 +0800

    还在写。

--- 新算法准确率评测/NewAlogrithmTest.py
+++ 新算法准确率评测/NewAlogrithmTest.py
@@ -3,21 +3,10 @@
 
 #hello.py
 import sys, getopt, codecs, os, subprocess, BusStat
-
-bus_relations = []
+import Kernal
 
 #是否一条龙
 one_dragon_service = False
-
-base_data_file_name = os.path.abspath('input/s_json.csv')
-#bus_file有默认参数
-bus_file_name = os.path.abspath('compare/bus_relations.csv')
-#排序结果文件
-tmp_file_name = os.path.abspath('temp/print_relate_bus_middle.tmp')
-sort_file_name = os.path.abspath('temp/print_relate_bus_middle.tmp.sort')
-#最终准备对拍的两个结果
-real_offline_result_name = os.path.abspath('compare/real_offline_result.csv')
-offline_result_name = os.path.abspath('compare/offline_result.csv')
 
 def IfContinueOn(tip):
     if one_dragon_service:
@@ -32,7 +21,7 @@ def IfContinueOn(tip):
         return True
 
 def usage():
-    print('Help!! Please put in "-b busfile_name -i inputfile_name -o output_file_name --dragon"!')
+    print('Help!! Please put in "--old_file=old_matching.log --new_file=new_matching.log --bus_relation_file=bus_rel.csv --basedata=input/s_json.csv --dragon"!')
 #解析命令行，来获取相应参数，具体见--help
 def parseParams():
     global one_dragon_service
@@ -63,7 +52,7 @@ def parseParams():
         sys.exit()
 
     print("CommandParam:")
-    print("old_file", old_file, "new_file", new_file, "bus_relation_file", bus_relation_file, "basedata:", basedata, "one_dragon_service", one_dragon_service)
+    print("old_file=", old_file, "new_file=", new_file, "bus_relation_file=", bus_relation_file, "basedata=", basedata, "one_dragon_service", one_dragon_service)
 
     return old_file, new_file, bus_relation_file, basedata
 
@@ -76,33 +65,32 @@ def init():
         os.makedirs('compare')
 
 #生成offline_result和 离线程序的result
-def generateCompareSample():
+def generateCompareSample(old_file_sorted, new_file_sorted, bus_relation_file, basedata):
 
     if not IfContinueOn('是否要生成对比Sample文件'):
         return
 
-    command_line = 'BusMatching.exe --offline --output --baseData ' + base_data_file_name + ' --inputFile ' + sort_file_name + ' --outputfile ' + offline_result_name
-    print('生成offline_result.csv: ' + command_line)
-    status = subprocess.call(command_line, shell=True)
-    if (status != 0):
-        print("Error: Program End.")
-        sys.exit(-1)
+    old_file_cmp = old_file_sorted+".cmp"
+    new_file_cmp = new_file_sorted+".cmp"
+    Kernal.generateRealOffLineResult(basedata=basedata, input_file=old_file_sorted, bus_rel=bus_relation_file, output=old_file_compare)
+    Kernal.generateRealOffLineResult(basedata=basedata, input_file=new_file_sorted, bus_rel=bus_relation_file, output=new_file_cmp)
 
-    command_line = 'BusMatchingResultGenerator.exe -m=0 -lon=10 -lat=11 -l=' + base_data_file_name + ' -i=' + sort_file_name + ' -b=' + bus_file_name+ ' -o=' + real_offline_result_name
-    print('生成judgement_result.csv: ' + command_line)
-    status = subprocess.call(command_line, shell=True)
-    if (status != 0):
-        print("Error: Program End.")
-        sys.exit(-1)
-        
+    return old_file_cmp, new_file_cmp
+
 #根据BusRelations来取出待排序的gps点，然后排序
 def generateSortedSample(old_file, new_file):
 
     if not IfContinueOn('是否要生成排好序的Sample文件'):
         return
     #排序tmp文件
-    sortFile(old_file)
-    sortFile(new_file)
+    Kernal.sortFile(old_file)
+    Kernal.sortFile(new_file)
+
+    old_file_sorted = old_file + ".sort"
+    new_file_sorted = new_file + ".sort"
+
+    return old_file_sorted, new_file_sorted
+
 
 if __name__=="__main__":
 
@@ -111,8 +99,9 @@ if __name__=="__main__":
 #解析命令行，来获取相应参数，具体见--help
     old_file, new_file, bus_relation_file, basedata = parseParams()
 #看看是否有必要生成排序好的Sample
-    generateSortedSample(old_file, new_file)
+    old_file_sorted, new_file_sorted = generateSortedSample(old_file, new_file)
 #产生对拍文件
-    generateCompareSample()
+    old_file_cmp, new_file_cmp = generateCompareSample(old_file_sorted, new_file_sorted, bus_relation_file, basedata)
 #统计正确率
-    BusStat.CountAccuracy(offline_result_name, real_offline_result_name)
+    BusStat.CountAccuracy(old_file_sorted, old_file_cmp)
+    BusStat.CountAccuracy(new_file_sorted, new_file_cmp)
