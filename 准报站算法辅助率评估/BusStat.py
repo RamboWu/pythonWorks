a4531dc3a7ae346cd4831fed8741a5475b85050c
Rambo Wu <RamboWu90@gmail.com>
2016-06-18 17:19:42 +0800

    增加了每辆车的统计

--- 准报站算法辅助率评估/BusStat.py
+++ 准报站算法辅助率评估/BusStat.py
@@ -54,27 +54,22 @@ class BusStat:
     def __init__(self,bus_id):
 
         self.bus_id = bus_id
-        self.correct = 0
-        self.correct_right = 0
+        self.total = 0
+        self.miss = 0
+        self.wrong = 0
 
-    def addStat(self, if_correct):
-        global TotalCorrect
-        global TotalCorrectRight
-        TotalCorrect += 1
-        self.correct += 1
-
-        if if_correct:
-            TotalCorrectRight += 1
-            self.correct_right += 1
-
-    def correctRate(self):
-        if self.correct != 0:
-            return float(self.correct_right) / float(self.correct)
+    def missRate(self):
+        if self.total != 0:
+            return float(self.miss) / float(self.total)
         else:
             return 0
 
     def report(self):
-        print(self.bus_id, ' 修正个数:', self.correct, '正确数:', self.correct_right, '准确率:', self.correctRate())
+        print(self.bus_id, \
+        'Total:', self.total, \
+        'Miss:', self.miss, \
+        'Wrong:', self.wrong, \
+        '丢失率:', self.missRate())
 
 class OneFileTest:
 
@@ -99,6 +94,8 @@ class OneFileTest:
 
         self.sample_file = sample_file
         self.cmp_file = cmp_file
+
+        self.BusMap = dict()
 
 
     def ReportTotalStat(self):
@@ -131,22 +128,36 @@ class OneFileTest:
         logger.info('准确的个数:%s', self.total_offline_assist_correct)
         logger.info('准确率:%s', float(self.total_offline_assist_correct)/float(self.total_offline_assist_can_cmp))
 
+        for key in self.BusMap.keys():
+            self.BusMap[key].report()
+
     def Judge(self, sample_line, cmp_line,lineno):
         sample_line = sample_line.strip()
         sample_line_tags = sample_line.split(',')
         count = sample_line.count(',') + 1
         cmp_line_tags = cmp_line.split(',')
 
+        if sample_line[3] in self.BusMap.keys():
+            bus_stat = self.BusMap.get(off_line_tags[3])
+        else:
+            bus_stat = BusStat(off_line_tags[3])
+            self.BusMap[off_line_tags[3]] = bus_stat
+
         self.total += 1
+        self.BusMap[off_line_tags[3]].total += 1
+
         if int(sample_line_tags[0]) == 1:
             self.total_correct += 1
             if int(cmp_line_tags[0]) == 1:
                 self.total_correct_can_cmp += 1
                 if sample_line_tags[4] == cmp_line_tags[4]:
                     self.total_correct_right += 1
+                else:
+                    self.BusMap[off_line_tags[3]].wrong += 1
 
         if int(sample_line_tags[0]) != 1 and int(cmp_line_tags[0]) == 1:
             self.total_correct_mis += 1
+            self.BusMap[off_line_tags[3]].miss += 1
 
         #统计准报站算法的使用率和准确率
         index = 18
