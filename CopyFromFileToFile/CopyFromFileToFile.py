6a2098c16e9b0ee63ca573ea659bc15ec3e7145d
Rambo Wu <RamboWu90@gmail.com>
2016-06-20 12:22:03 +0800

    增加了从中间截取的功能

--- CopyFromFileToFile/CopyFromFileToFile.py
+++ CopyFromFileToFile/CopyFromFileToFile.py
@@ -3,35 +3,39 @@
 #hello.py
 import sys, getopt, codecs
 
-def copyFromFileToFile(src_file_name, dest_file_name, lines, somestring):
+def copyFromFileToFile(src_file_name, dest_file_name, lines, somestring, start = 0):
     src_file = codecs.open(src_file_name, 'r', 'utf-8')
     dest_file = codecs.open(dest_file_name, 'w', 'utf-8')
-    
-    line = src_file.readline() 
-    i = 0;
-    while line:  
-        i += 1
-        if i>lines:
-            break
 
-        if (line.find(somestring) >= 0):
-            dest_file.write(line)
-        
+    line = src_file.readline()
+    i = 0;
+    num = 0;
+    while line:
+        i += 1
+        if (i >= start):
+            num+= 1
+            if num>lines:
+                break
+
+            if (line.find(somestring) >= 0):
+                dest_file.write(line)
+
         line = src_file.readline()
 
     src_file.close();
     dest_file.close();
 
 def usage():
-    print('Help!! Please put in "-i inputfile_name -o output_file_name -l 10000 -c somestring"!')
+    print('Help!! Please put in "-i inputfile_name -o output_file_name -l 10000 -c somestring -s 50"!')
 
 def parseParams():
-    opts, args = getopt.getopt(sys.argv[1:], "hi:o:l:c:")
+    opts, args = getopt.getopt(sys.argv[1:], "hi:o:l:c:s:")
 
     input_file=""
     output_file="output"
     lines = 1000
     somestring = "";
+    start = 0
 
     for op, value in opts:
         if op == "-i":
@@ -42,6 +46,8 @@ def parseParams():
             lines = int(value)
         elif op == "-c":
             somestring = value
+        elif op == "-s":
+            start = int(value)
         elif op == "-h":
             usage()
             sys.exit()
@@ -50,12 +56,12 @@ def parseParams():
         usage()
         sys.exit()
 
-    print("input", input_file, "output", output_file, "line", lines, "contain", somestring)
+    print("input", input_file, "output", output_file, "line", lines, "contain", somestring, "start from:", start)
 
-    return input_file, output_file, lines, somestring
+    return input_file, output_file, lines, somestring, start
 
 if __name__=="__main__":
 
-    input_file, output_file, lines, somestring = parseParams()
+    input_file, output_file, lines, somestring, start = parseParams()
 
-    copyFromFileToFile(input_file, output_file, lines, somestring)
+    copyFromFileToFile(input_file, output_file, lines, somestring, start)
