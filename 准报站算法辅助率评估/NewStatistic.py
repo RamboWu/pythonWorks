9e01ebcc9859d3b2003d86248aa614eafcf5e9dc
Rambo Wu <RamboWu90@gmail.com>
2016-06-24 15:30:17 +0800

    增加了命令行

--- 准报站算法辅助率评估/NewStatistic.py
+++ 准报站算法辅助率评估/NewStatistic.py
@@ -2,10 +2,11 @@
 #!/usr/bin/python
 
 #NewStatistic.py
-import codecs, sys, getopt, os
+import codecs, sys, getopt, os, subprocess
 
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0,parentdir)
+from Util.CommandManager import Manager
 from Util.Business import OnlineOfflineGPSFileReader
 from Util.Business import OnlineResCount
 from Util.Business import OnlineResAssistCount
@@ -36,5 +37,17 @@ def StartStatistic(sample_file, cmp_file):
     file_reader.startCount(sample_file=sample_file,cmp_file=cmp_file)
     file_reader.Report()
 
+manager = Manager()
+@manager.option('-i', '--input', dest='input_file', required=True)
+@manager.option('-j', '--judge', dest='judge_file', required=True)
+def run(input_file=None, judge_file = None):
+    StartStatistic(input_file, judge_file)
+
+@manager.command
+def test():
+    command_line = 'python3 NewStatistic.py run -i test/matching.log.sort-100000 -j test/matching.log.sort.cmp-100000'
+    print(command_line)
+    status = subprocess.call(command_line, shell=True)
+
 if  __name__ ==  '__main__':
-    StartStatistic(sample_file='test/matching.log.sort-100000',cmp_file='test/matching.log.sort.cmp-100000')
+    manager.run()
