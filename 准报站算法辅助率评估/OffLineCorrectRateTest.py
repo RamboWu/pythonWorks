605dea927bcd9be776fc300568ec2a93c5519bfb
Rambo Wu <RamboWu90@gmail.com>
2016-07-06 17:08:11 +0800

    增加测试

--- 准报站算法辅助率评估/OffLineCorrectRateTest.py
+++ 准报站算法辅助率评估/OffLineCorrectRateTest.py
@@ -11,14 +11,19 @@ from Util.Tools import FileHelper
 manager = Manager()
 @manager.command
 def test():
+    print('Cast1:')
     command_line = 'python3 OffLineCorrectRate.py run -i test/matching.log1 --bus_relation_file=test/bus_rel.csv1 --basedata=test/s_json.csv1'
     print(command_line)
     status = subprocess.call(command_line, shell=True)
 
-    command_line = 'python3 OffLineCorrectRate.py run -i test/matching.log1 --bus_relation_file=test/bus_rel.csv1 --basedata=test/s_json.csv1 --dragon=True'
+    print('Cast2:')
+    command_line = 'python3 OffLineCorrectRate.py run -i test/test2/42255.csv1 --bus_relation_file=test/test2/single.csv1 --basedata=test/test2/s_json.csv --dragon=True'
     print(command_line)
     status = subprocess.call(command_line, shell=True)
 
+@manager.command
+def testfile():
+    print('Cast3:')
     command_line = 'python3 OffLineCorrectRate.py count -i test/matching.log.sort-100000 -j test/matching.log.sort.cmp-100000'
     print(command_line)
     status = subprocess.call(command_line, shell=True)
