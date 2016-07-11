f25a0fc0857a199fe08614bac5a18e5149ad2970
Rambo Wu <RamboWu90@gmail.com>
2016-07-11 19:32:45 +0800

    添加了pickup

--- PickUp/PickUp.py
+++ PickUp/PickUp.py
@@ -1,52 +1,48 @@
 #  -*- coding: utf-8 -*-
 #!/usr/bin/python
 
-import sys, getopt, codecs, os
-import datetime
-from apscheduler.schedulers.blocking import BlockingScheduler
+import sys, getopt, codecs, os, subprocess
 
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0,parentdir)
 from Util.CommandManager import Manager
 
 
+def pickup(input_file_name, keys, index, output_file):
+    if not os.path.exists(input_file_name):
+        print(input_file_name + ' not exists')
+        return
 
+    if not os.path.exists(os.path.dirname(output_file)):
+        os.makedirs(os.path.dirname(output_file))
 
-def check():
-    #检查一些必要的测试文件是否存在
-    pass
+    keys = keys.split(',')
+    input_file = codecs.open(input_file_name, 'r', 'utf-8')
+    dest_file = codecs.open(output_file, 'w', 'utf-8')
+    line = input_file.readline()
+    while line:
+        tags = line.strip().split(',')
+        if len(tags) >= index and tags[index] in keys:
+            dest_file.write(line)
 
-def dailytest():
-    #一些统计
-    pass
+        line = input_file.readline()
 
-
-dailytest_dirs = None
-
-#@schedudler.cron_schedule(hour=17, minute=5, second=0)
-def dailytest():
-    print('dailytest at:', datetime.datetime.now())
-    dirs = dailytest_dirs.split(',')
-    for _dir in dirs:
-        
-
-schedudler = BlockingScheduler(daemonic = False)
-schedudler.add_job(dailytest, 'cron', id='dailytest', hour=1, minute=0, second=0)
+    input_file.close()
+    dest_file.close()
 
 manager = Manager()
-@manager.option('-i', '--input_dirs', dest='input_dirs', required=True)
-@manager.option('--immediate', dest='immediate', default = False)
-def run(input_dirs = None, immediate = False):
-    global dailytest_dirs
-    print('input_dirs', input_dirs, 'immediate', immediate)
-    dailytest_dirs = input_dirs
-    check()
-    #接受要测试的几个目录，文件名
-    #定时统计 dailytest
-    if immediate:
-        dailytest()
-    schedudler.start()
+@manager.option('-i', '--input', dest='input_file', required=True)
+@manager.option('-k', '--keys', dest='keys', required = True)
+@manager.option('-n', '--index', dest='index', default = 3)
+@manager.option('-o', '--output', dest='output_file', default = 'output')
+def run(input_file = None, keys = None, index = 3, output_file = 'output'):
+    pickup(input_file, keys, index, output_file)
+
+@manager.command
+def test():
+    command_line = 'python3 PickUp.py run -i test/input/test.csv1 -k 078113,078114 -o test/output/output.csv'
+    print(command_line)
+    status = subprocess.call(command_line, shell=True)
 
 if __name__ == "__main__":
-
     manager.run()
