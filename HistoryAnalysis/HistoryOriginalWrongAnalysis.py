d738f8db884751f2eda406b3e486b15a17717c98
Rambo Wu <RamboWu90@gmail.com>
2016-08-04 14:59:10 +0800

    更加精简了一点

--- HistoryAnalysis/HistoryOriginalWrongAnalysis.py
+++ HistoryAnalysis/HistoryOriginalWrongAnalysis.py
@@ -20,11 +20,6 @@ class BusLineInfo:
         self.original_line_id = ''
         self.real_line_id = ''
 
-class OriginalWrongCount:
-    def __init__(self):
-        self.same_count = 0
-        self.diff_count = 0
-
 def getBusRelations(bus_relation_file):
 
     bus_file = codecs.open(bus_relation_file, 'r', 'utf-8')
@@ -46,7 +41,6 @@ def getBusRelations(bus_relation_file):
 
 class HistoryOriginalWrongAnalysis:
     def __init__(self):
-        self.BusMap = dict()
         self.analysis_helper = None
 
     def readCity(self, city_dir):
@@ -64,25 +58,11 @@ class HistoryOriginalWrongAnalysis:
             if os.path.isdir(filepath):  #如果filepath是目录，则再列出该目录下的所有文件
                 self.readOneDayFlight(filepath, line)
 
-        self.report()
-
-    def report(self):
-        total_same = 0
-        total_diff = 0
-        for bus_id in self.BusMap.keys():
-            total_same += self.BusMap[bus_id].same_count
-            total_diff += self.BusMap[bus_id].diff_count
-
-        print('same: %s, diff:%s, samePer:%s'%(total_same, total_diff, MathHelper.percentToString(total_same, total_same + total_diff)))
+        self.analysis_helper.report()
 
     def readOneDayFlight(self, dir, title):
         flight_file = os.path.join(dir, 'busline_busLineAll.log')
         if os.path.exists(flight_file):
             bus_relations = getBusRelations(flight_file)
             for key in bus_relations.keys():
-                if not key in self.BusMap.keys():
-                    self.BusMap[key] = OriginalWrongCount()
-                same_count, diff_count = \
-                    self.analysis_helper.analysisSameLineFrontCount(bus_relations[key].real_line_id, bus_relations[key].original_line_id)
-                self.BusMap[key].same_count += same_count
-                self.BusMap[key].diff_count += diff_count
+                self.analysis_helper.analysisSameLineFrontCountWithBusId(key, bus_relations[key].real_line_id, bus_relations[key].original_line_id)
--- HistoryAnalysis/OriginalWrongAnalysisHelper.py
+++ HistoryAnalysis/OriginalWrongAnalysisHelper.py
@@ -42,8 +42,14 @@ def getLineFrontSimilar(file_):
 
     return relations
 
+class OriginalWrongCount:
+    def __init__(self):
+        self.same_count = 0
+        self.diff_count = 0
+
 class OriginalWrongAnalysisHelper:
     def __init__(self, line_front_similar_file):
+        self.BusMap = dict()
         self.line_front_similar = getLineFrontSimilar(line_front_similar_file)
 
     def analysisSameLineFrontCount(self, one_line_id, other_line_id):
@@ -61,4 +67,22 @@ class OriginalWrongAnalysisHelper:
                 return 0, 1
             #print(today_line.real_line_id, today_line.original_line_id, 1, 0)
             return 1, 0
-            
+
+    def report(self):
+        total_same = 0
+        total_diff = 0
+        for bus_id in self.BusMap.keys():
+            total_same += self.BusMap[bus_id].same_count
+            total_diff += self.BusMap[bus_id].diff_count
+
+        print('AnalysisHelper: same: %s, diff:%s, samePer:%s'\
+            %(total_same, total_diff, MathHelper.percentToString(total_same, total_same + total_diff)))
+
+    def analysisSameLineFrontCountWithBusId(self, bus_id, one_line_id, other_line_id):
+        same_count, diff_count = self.analysisSameLineFrontCount(one_line_id, other_line_id)
+
+        if not bus_id in self.BusMap.keys():
+            self.BusMap[bus_id] = OriginalWrongCount()
+
+        self.BusMap[bus_id].same_count += same_count
+        self.BusMap[bus_id].diff_count += diff_count
